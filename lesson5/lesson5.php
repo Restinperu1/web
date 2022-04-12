f6b5dd6611163bbe405e0b2866592df218fce3fb
Eymen <eymenadak30@gmail.com>
2022-04-12 07:32:58 +0300

    Lesson5

---
+++ lesson5/lesson5.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+
+$studentInfo = array(
+      "ogr1" => array(
+    "id" => 1,
+    "name" => "Eymen",
+    "lastname" => "Adak",
+    "depertmant" => "Computer Science",
+    "lessons" => array(
+        "lesson1" => "Programming",
+        "lesson2" => "Mobil App Programming",
+        "lesson3" => "Database"
+    ),
+    "class" => 1,
+    "from" => "Ankara"),
+
+    "ogr2" => array(
+        "id" => 1,
+        "name" => "Eymen",
+        "lastname" => "Adak",
+        "depertmant" => "Computer Science",
+        "lessons" => array(
+            "lesson1" => "Programming",
+            "lesson2" => "Mobil App Programming",
+            "lesson3" => "Database"
+        ),
+        "class" => 1,
+        "from" => "Ankara"),
+
+        "ogr3" => array(
+            "id" => 1,
+            "name" => "Eymen",
+            "lastname" => "Adak",
+            "depertmant" => "Computer Science",
+            "lessons" => array(
+                "lesson1" => "Programming",
+                "lesson2" => "Mobil App Programming",
+                "lesson3" => "Database"
+            ),
+            "class" => 1,
+            "from" => "Ankara")
+    
+);
+
+
+
+
+
+
+
+?>
+    
+</body>
+</html>
