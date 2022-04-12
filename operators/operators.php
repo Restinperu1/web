2ab0ac84666ec06181cec06c15a6e60bc2b86b55
Eymen <eymenadak30@gmail.com>
2022-04-12 07:30:49 +0300

    Operators

---
+++ operators/operators.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   
+    <?php
+
+            // BOOLEANS
+    
+   /*      
+
+        $isComplate = (string) true;
+
+        var_dump($isComplate);
+
+        if($isComplate){
+            //do something
+            echo "success";
+        }
+
+        else {
+            //do something else
+            echo "fail";
+        }                         */
+
+
+               //INTEGERS
+
+/*          // $x = 00000021;
+
+            // echo $x;
+
+            // $x = (int) PHP_INT_MAX;
+
+            // $x = (int) '87';
+            
+            // $x = null;
+
+            // $x = -23;
+            
+            // var_dump($x);      */
+
+                //STRINGS
+/* 
+            // $name = 'John'
+            // $Surname = 'Mosby'
+            // $StringNumber = "55"
+            
+            // var_dump($name);    */
+
+
+
+    ?>
+
+</body>
+</html>
